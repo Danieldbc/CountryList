043ca62563f76237046e28444200c1e81e9a14c4
Danieldbc <Danieldbc@github.com>
2017-10-13 10:44:51 +0200

    Initial commit

---
+++ app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/countrylist/CountryListActivity.java
@@ -0,0 +1,13 @@
+package edu.eseiaat.upc.pma.borreguero.daniel.countrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+    }
+}
